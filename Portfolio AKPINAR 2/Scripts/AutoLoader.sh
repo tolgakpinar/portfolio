8b8f518296c019ebc23d9e6825cde0bea805e412
tolgakpinar <113177071+tolgakpinar@users.noreply.github.com>
2024-03-28 02:37:03 +0100

    Delete Portfolio AKPINAR 2 directory

--- Portfolio AKPINAR 2/Scripts/AutoLoader.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Reload the JSON file
-php -f /home/unixcop-user/Scripts/traitement.php
--- Portfolio AKPINAR 2/Scripts/bdd_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $server = "localhost";
-  $user = "tolga";
-  $password = "tolga";
-  $bdd = "GestRetards";
-  $apiKey = 'apiKey: xswDsiPruJB4iyMHUu7v2GsMbzCBgot6'; // CHANGER AVEC lE JETON DE L'ENTREPRISE
-?>

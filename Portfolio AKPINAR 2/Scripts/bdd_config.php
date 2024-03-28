54ac0eda4181971c9251c0f69ea05afb4bf2017f
tolgakpinar <113177071+tolgakpinar@users.noreply.github.com>
2024-03-28 02:06:27 +0100

    Add files via upload

---
+++ Portfolio AKPINAR 2/Scripts/bdd_config.php
@@ -0,0 +1,7 @@
+<?php
+  $server = "localhost";
+  $user = "tolga";
+  $password = "tolga";
+  $bdd = "GestRetards";
+  $apiKey = 'apiKey: xswDsiPruJB4iyMHUu7v2GsMbzCBgot6'; // CHANGER AVEC lE JETON DE L'ENTREPRISE
+?>
